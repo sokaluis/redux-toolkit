dcf2f3e212653e14fca7ead2ecbe0e0748fe0dd1
Luis Azocar <>
2022-07-01 19:56:41 -0300

    testing RTK Query

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { Provider } from "react-redux";
 import { store } from "./store";
 // import App from "./App";
 import PokemonApp from "./PokemonApp";
+import TODOApp from "./TODOApp";
 // CSS
 import "./index.css";
 
@@ -11,7 +12,7 @@ ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Provider store={store}>
       {/* <App /> */}
-      <PokemonApp />
+      <TODOApp />
     </Provider>
   </React.StrictMode>
 );
